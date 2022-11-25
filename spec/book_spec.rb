0250420d1839b3526af66c2899116d4984029890
Diego <godie8a@hotmail.com>
2022-11-24 21:35:48 -0500

    fixed Linters problem4

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,34 +1,21 @@
 require_relative '../model/book'
+require 'date'
 
-RSpec.describe Book do
+describe Book do
   before :each do
-    @book = Book.new(4846, true, '2013/03/12', 'edith', 'oga')
+    @book = Book.new(cover_state: 'bad', publisher: nil)
   end
 
-  it 'Create an instance of the book object' do
-    expect(@book).to be_an_instance_of(Book)
+  it 'should return bad' do
+    expect(@book.cover_state).to eq('bad')
   end
 
-  it 'To be 1 kind on the Item' do
-    expect(@book).to be_kind_of(Item)
+  it 'should return false' do
+    expect(@book.archived).to be(false)
   end
 
-  it 'Add label to a book' do
-    label = Label.new(3, 'Classicals', 'Blue')
-    label.add_item(@book)
-    expect(@book.label.color).to be == 'Blue'
-  end
-
-  it 'creates a JSON string' do
-    label = Label.new(3, 'Classicals', 'Blue')
-    label.add_item(@book)
-    json = JSON.generate(@book)
-    expect(json).to be == '{"json_class":"Book","data":[2022,true,"2013/03/12","Edith","oga"]}'
-  end
-
-  it 'creates instance from JSON string' do
-    json = '{"json_class":"Book","data":[2022,true,"2013/03/12","Edith","oga"]}'
-    new_book = JSON.parse(json, create_additions: true)
-    expect(new_book).to be_an_instance_of(Book)
+  it 'should return true' do
+    @book.move_to_archive
+    expect(@book.archived).to be(true)
   end
 end
